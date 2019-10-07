341a984e83aa25bf960a604f844679198910bcaf
Josué Moreno <josue.moreno@forteinnovation.mx>
2019-10-07 18:12:40 -0500

    modelo de base de datos

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Tb_Usuario(models.Model):
+    
+  telefono = models.CharField(max_length=50)
+  password = models.CharField(max_length=100)
+  nombre = models.CharField(max_length=100)
+  edad = models.IntegerField(default=18)
+  apellidoMaterno = models.CharField(max_length=100)
+  apellidoPaterno = models.CharField(max_length=100)
+  status = models.BooleanField(default=1)
+
+  def __str__(self):
+    return self.nombre 
+
+class Tb_CatalogoServicio(models.Model):
+    
+  nombreServicio = models.IntegerField(default=0)
+  status =  models.BooleanField(default=1)
+
+  def __str__(self):
+    return self.nombreServicio
+
+class tb_CalendarioServicio(models.Model):
+    
+  idUsuario = models.IntegerField(default=0)
+  idServicio = models.IntegerField(default=0)
+  status =  models.BooleanField(default=1)
+
+  def __str__(self):
+    return self.idUsuario
